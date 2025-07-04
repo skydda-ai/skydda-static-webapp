6eb9a1c26cdf60bc0af84ebef434993c14891f65
Rishon Krishna <rishon@keyvalue.systems>
2025-07-04 16:44:01 +0530

    task: website hone page

---
+++ src/components/button.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  variant?: 'filled' | 'outline';
+  children: React.ReactNode;
+};
+
+const baseStyles =
+  'font-inter px-5 py-3 rounded-3xl font-medium text-base cursor-pointer';
+
+const variants = {
+  filled: 'bg-[linear-gradient(to_right,_#45266A,_#632CA7)] text-white',
+  outline: 'border border-white text-white'
+};
+
+export const Button: React.FC<ButtonProps> = ({
+  variant = 'filled',
+  children,
+  className = '',
+  ...props
+}) => (
+  <button
+    className={`${baseStyles} ${variants[variant]} ${className}`}
+    {...props}
+  >
+    {children}
+  </button>
+);
+
+export default Button;
