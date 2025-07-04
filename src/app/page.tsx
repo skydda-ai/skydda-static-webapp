088100c3f415fe020a33d42d22c077b3d6f01b29
RishonKeyvalue <112230540+RishonKeyvalue@users.noreply.github.com>
2025-07-04 18:41:41 +0530

    Task/website home page (#2)
    * task: website initial commit next, eslint, prettier, husky
    
    * task: website initial commit next, eslint, prettier, husky
    
    * task: website initial commit next, eslint, prettier, husky
    
    * task: website initial commit next, eslint, prettier, husky
    
    * task: husky set up
    
    * task: husky set up
    
    * task: husky set up
    
    * task: husky set up
    
    * task: husky set up
    
    * task: husky set up
    
    * task: skydda website repo
    
    * task: website hone page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,104 +1,45 @@
-import Image from "next/image";
-// console.log('Hello from page.tsx');
+import Button from '@/components/button';
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      <main className='flex flex-col gap-[32px] row-start-2 items-center sm:items-start'>
-        <Image
-          className='dark:invert'
-          src='/next.svg'
-          alt='Next.js logo'
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className='list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]'>
-          <li className='mb-2 tracking-[-.01em]'>
-            Get started by editing{" "}
-            <code className='bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold'>
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className='tracking-[-.01em]'>
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className='relative min-h-screen p-0 md:p-5 bg-[#0D0D0D] overflow-hidden flex'>
+      <div className="flex-1 bg-[url('/images/home_bg.png')] bg-no-repeat bg-cover bg-center rounded-3xl px-[30px] md:px-[70px] lg:px-[90px] xl:px-[110px] py-[50px] md:py-[90px] lg:py-[110px] xl:py-[130px]">
+        <header className='relative z-10'>
+          <div className='flex items-center justify-center md:justify-start gap-3'>
+            <img src='/logo/skydda_logo.svg' alt='Skydda Logo' />
+          </div>
+        </header>
 
-        <div className='flex gap-4 items-center flex-col sm:flex-row'>
-          <a
-            className='rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto'
-            href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            <Image
-              className='dark:invert'
-              src='/vercel.svg'
-              alt='Vercel logomark'
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className='rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]'
-            href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className='row-start-3 flex gap-[24px] flex-wrap items-center justify-center'>
-        <a
-          className='flex items-center gap-2 hover:underline hover:underline-offset-4'
-          href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <Image
-            aria-hidden
-            src='/file.svg'
-            alt='File icon'
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className='flex items-center gap-2 hover:underline hover:underline-offset-4'
-          href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <Image
-            aria-hidden
-            src='/window.svg'
-            alt='Window icon'
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className='flex items-center gap-2 hover:underline hover:underline-offset-4'
-          href='https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <Image
-            aria-hidden
-            src='/globe.svg'
-            alt='Globe icon'
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <main className='relative z-10 flex items-center flex-col gap-[60px] md:flex-row md:gap-[80px] mt-[60px]'>
+          <div className='flex flex-col justify-center gap-[58px]'>
+            <h1 className='flex flex-col justify-center font-jakarta text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold text-white w-fit'>
+              REIMAGINING THE SOC
+              <br />
+              <span className='font-normal'>FROM THE GROUND UP</span>
+            </h1>
+
+            <div className='flex flex-col md:flex-row gap-4'>
+              <Button>Apply for early access</Button>
+              <Button variant='outline'>Join our team</Button>
+            </div>
+          </div>
+
+          <div className='flex flex-1 justify-center items-center'>
+            <div className='relative w-[230px] md:w-[460px]'>
+              <div className='absolute inset-0 w-[230px] md:w-[460px] bg-[linear-gradient(127.9deg,_rgba(148,_62,_250,_0.7)_1.58%,_rgba(210,_77,_228,_0.7)_98.42%)] mix-blend-color pointer-events-none z-0'></div>
+              <video
+                src='/gif/skydda_icon.mp4'
+                autoPlay
+                loop
+                muted
+                className='w-[230px] md:w-[460px]'
+              />
+            </div>
+          </div>
+        </main>
+      </div>
     </div>
   );
-}
+};
+
+export default Home;
