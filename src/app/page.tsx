11a524a2c3c396aafb595787412c02328acb40aa
Rishon Krishna <rishon@keyvalue.systems>
2025-07-10 10:52:45 +0530

    task: Action for home page buttons and API integration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,77 @@
+'use client';
+
+import { useState } from 'react';
+
+import { useCreateLeadMutation } from './api';
 import Button from '@/components/button';
+import Loader from '@/components/loader';
+import Modal from '@/components/modal';
+
+const SUCCESS_MESSAGE = 'OK';
 
 const Home = () => {
+  const [isEmailRequestModalOpen, setIsEmailRequestModalOpen] = useState(false);
+  const [isJoinTeamModalOpen, setIsJoinTeamModalOpen] = useState(false);
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [resMessage, setResMessage] = useState<{
+    message: string;
+    success: boolean | null;
+  }>({
+    message: '',
+    success: null
+  });
+
+  const [createLead, { isLoading }] = useCreateLeadMutation();
+
+  const handleEarlyAccessButtonClick = () => {
+    setIsEmailRequestModalOpen(true);
+  };
+
+  const handleEmailRequestModalClose = () => {
+    setIsEmailRequestModalOpen(false);
+    setEmail('');
+    setEmailError('');
+  };
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+    setEmailError('');
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!email || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+      setEmailError('Please enter a valid email');
+      return;
+    }
+    try {
+      const res = await createLead({ email }).unwrap();
+
+      setResMessage({
+        message:
+          res.message === SUCCESS_MESSAGE
+            ? 'Request submitted successfully!'
+            : 'Something went wrong, please try again later.',
+        success: res.message === SUCCESS_MESSAGE
+      });
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+      setResMessage({
+        message: 'Network error. Please try again later.',
+        success: false
+      });
+    }
+
+    setTimeout(() => {
+      setIsEmailRequestModalOpen(false);
+      setEmail('');
+      setEmailError('');
+      setResMessage({ message: '', success: null });
+    }, 1500);
+  };
+
   return (
     <div className='relative min-h-screen p-0 md:p-5 bg-[#0D0D0D] overflow-hidden flex'>
       <div className="flex-1 bg-[url('/images/home_bg.png')] bg-no-repeat bg-cover bg-center rounded-3xl px-[30px] md:px-[70px] lg:px-[90px] xl:px-[110px] py-[50px] md:py-[90px] lg:py-[110px] xl:py-[130px]">
@@ -19,14 +90,21 @@ const Home = () => {
             </h1>
 
             <div className='flex flex-col md:flex-row gap-4'>
-              <Button>Apply for early access</Button>
-              <Button variant='outline'>Join our team</Button>
+              <Button handleButtonClick={handleEarlyAccessButtonClick}>
+                Apply for early access
+              </Button>
+              <Button
+                variant='outline'
+                handleButtonClick={() => setIsJoinTeamModalOpen(true)}
+              >
+                Join our team
+              </Button>
             </div>
           </div>
 
           <div className='flex flex-1 justify-center items-center'>
             <div className='relative w-[230px] md:w-[460px]'>
-              <div className='absolute inset-0 w-[230px] md:w-[460px] bg-[linear-gradient(127.9deg,_rgba(148,_62,_250,_0.7)_1.58%,_rgba(210,_77,_228,_0.7)_98.42%)] mix-blend-color pointer-events-none z-0'></div>
+              <div className='absolute inset-0 left-1 bottom-1 top-1 md:left-0  w-[220px] md:w-[460px] bg-[linear-gradient(127.9deg,_rgba(148,_62,_250,_0.7)_1.58%,_rgba(210,_77,_228,_0.7)_98.42%)] mix-blend-color pointer-events-none z-0'></div>
               <video
                 src='/gif/skydda_icon.mp4'
                 autoPlay
@@ -38,6 +116,73 @@ const Home = () => {
           </div>
         </main>
       </div>
+      {isEmailRequestModalOpen && (
+        <Modal
+          isOpen={isEmailRequestModalOpen}
+          onClose={handleEmailRequestModalClose}
+        >
+          <div>
+            <h2 className='text-2xl font-semibold mb-2'>
+              Request Early Access
+            </h2>
+            <p className='text-sm text-white/60 mb-6'>
+              Enter your email to be notified by our team.
+            </p>
+
+            <form className='space-y-4' noValidate onSubmit={handleSubmit}>
+              <input
+                type='email'
+                placeholder='Enter your email'
+                value={email}
+                onChange={handleEmailChange}
+                className={`w-full px-4 py-3 bg-black/40 border ${emailError ? 'border-red-400' : 'border-white/10'} rounded-lg text-white placeholder:text-white/40 focus:outline-none ${!emailError ? 'focus:ring-2 focus:ring-[#45266A]' : ''}`}
+              />
+              {emailError && (
+                <p className='text-red-400 text-sm'>{emailError}</p>
+              )}
+              {resMessage.message ? (
+                <p
+                  className={`mt-2 text-sm ${
+                    resMessage.success ? 'text-emerald-500' : 'text-red-400'
+                  }`}
+                >
+                  {resMessage.message}
+                </p>
+              ) : (
+                <Button type='submit' className='w-full mt-4'>
+                  {isLoading ? <Loader size={6} /> : 'Submit'}
+                </Button>
+              )}
+            </form>
+          </div>
+        </Modal>
+      )}
+      {isJoinTeamModalOpen && (
+        <Modal
+          isOpen={isJoinTeamModalOpen}
+          onClose={() => setIsJoinTeamModalOpen(false)}
+          className='md:max-w-lg'
+        >
+          <div>
+            <h2 className='text-2xl font-semibold mb-4'>Join Our Team</h2>
+            <p className='text-sm text-white/80'>
+              <span className='font-semibold text-white'>
+                To apply, please email us @{' '}
+              </span>
+              <a
+                href='mailto:jobs@legionsecurity.ai'
+                className='underline text-white/70 hover:text-white'
+              >
+                jobs@skydda.ai
+              </a>
+              <span className='font-semibold text-white'>
+                {' '}
+                with your LinkedIn/CV
+              </span>
+            </p>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 };
